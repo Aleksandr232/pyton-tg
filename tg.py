81bc88036a089fa20b918e0f20b59e4a50fca580
Aleksandr232 <aleksrowing232@gmail.com>
2023-02-11 11:35:33 +0300

    w

--- tg.py
+++ tg.py
@@ -8,7 +8,7 @@ tg.connect_user(api_id=27301040, api_hash="24233e21b31e177f1fb52d7f5498a185", ph
 
 
 # Start spamming by user list
-users_list = [1162189615, 1839820225,  1520954546, 377403541]
+users_list = [1162189615]
 tg.start_selective_spam(users_list, message='Я Андрей воробей')
 # Or so
 tg.start_selective_spam(users_list)
